a776c53198b5cac002e759a32684d36f5b2433d5
Derek <Derek@MSI>
2020-06-11 16:27:01 -0700

    Intro to stack, undo redo

--- src/_02_Integer_Stack/IntegerStack.java
+++ src/_02_Integer_Stack/IntegerStack.java
@@ -2,48 +2,54 @@ package _02_Integer_Stack;
 
 public class IntegerStack {
 	//1. create a private array of integers
-	
+	private int [] data; 
 	//2. complete the constructor by initializing the member array
 	//   to a new array of length 0. This prevents null pointer exceptions.
-	
+	IntegerStack(){
+		data = new int[0];
+	}
 	
 	//3. Complete the steps in the push method.
 	public void push(int v) {
 		//A. create a new array that is one element larger than the member array
-		
+		int [] push = new int[data.length+1];
 		//B. set the last element of the new array equal to the integer passed into the method
-		
+		push[push.length -1 ]= v;
 		//C. iterate through the member array and copy all the elements into the new array
-		
+		for (int i = 0; i < data.length; i++) {
+			 push[i] = data[i];
+		}
 		//D. set the member array equal to the new array.
-		
+		data = push;
 	}
 	
 	//4. Complete the steps in the pop method.
 	public int pop() {
 		//A. create an integer variable and initialize it to the
 		//   last element of the member array.
-		
+		int s = data[data.length-1];
 		//B. create a new array that is one element smaller than the member array
-		
+		int [] pop = new int[data.length-1];
 		//C. iterate through the new array and copy every element from the
 		//   member array to the new array
-		
+		for (int i = 0; i < pop.length; i++) {
+			pop[i] = data[i];
+		}
 		//D. set the member array equal to the new array
-		
+		data = pop;
 		//E. return the variable you created in step A
-		return 0;
+		return s;
 	}
 	
 	//5. Complete the clear method to set the
 	//   member array to a new array of length 0
 	public void clear() {
-		
+		data = new int[0];
 	}
 	
 	//6. Complete the size method to return 
 	//   the length of the member array
 	public int size() {
-		return 0;
+		return data.length;
 	}
-}
+}	
--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -1,6 +1,19 @@
 package _03_IntroToStacks;
 
-public class _02_TextUndoRedo {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class _02_TextUndoRedo implements KeyListener{
+	JFrame frame;
+	JPanel panel;
+	JLabel label;
+	Stack<Character> text = new Stack<Character>();
+	Stack<Character> undo = new Stack<Character>();
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
@@ -13,6 +26,62 @@ public class _02_TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
+	public static void main(String[] args) {
+		new _02_TextUndoRedo().frame();
+	}
 	
+	void frame() {
+		frame = new JFrame();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panel = new JPanel();
+		frame.add(panel);
+		label = new JLabel();
+		panel.add(label);
+		frame.addKeyListener(this);
+	}
+	
+	private void makeLabelForStack() {
+		String word = "";
+	for (int i = 0; i < text.size(); i++) {
+		word += (text.get(i));
+	}	
+	label.setText(word);
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getKeyChar()== KeyEvent.VK_BACK_SPACE) {
+			if(text.size()>0) {
+			undo.push(text.pop());
+			}
+		}
+		else {
+		text.push(e.getKeyChar());
+		}
+		makeLabelForStack();
+		frame.pack();
+		frame.repaint();
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getKeyCode()== 17) {
+			System.out.println("eeqw");
+			if(undo.size()>0) {
+			text.push(undo.pop());
+			}
+		}
+		makeLabelForStack();
+		frame.pack();
+		frame.repaint();
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
