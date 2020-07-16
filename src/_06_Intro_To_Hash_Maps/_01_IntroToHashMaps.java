3c794ed8ecdba58cf3861cba368f2bd6cfaeac2a
Derek <Derek@windows10.microdone.cn>
2020-07-16 16:26:09 -0700

    String integer pair, intro to hashmaps

--- src/_06_Intro_To_Hash_Maps/_01_IntroToHashMaps.java
+++ src/_06_Intro_To_Hash_Maps/_01_IntroToHashMaps.java
@@ -1,16 +1,28 @@
 package _06_Intro_To_Hash_Maps;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class _01_IntroToHashMaps {
+	
 	public static void main(String[] args) {
+		
 		//1. Create a HashMap called roots with Integers for the keys and Doubles for the values.
-
+		HashMap<Integer, Double> root = new HashMap<Integer, Double>();
 		//2. Using a for-loop, add 500 entries to your HashMap. The key entry will be the 
 		//   current iteration of the loop (i). The value entry will be the square root of i.
-
+		for (int i = 0; i < 500; i++) {
+			root.put(i, Math.sqrt(i));
+		}
 		//3. Iterate through all the entries in your HashMap displaying the keys with their respective
 		//   square roots (values). Use the following format.
+		ArrayList<Double> s = new ArrayList<Double>();
+		for (Double i : root.values()) {
+			s.add(i);
+		}
+		for(Integer i : root.keySet()){
+			System.out.println("The square root of " + i + " is " + s.get(i));
+		}
 
 		//   The square Root of 0 is 0.0
 		//   The square Root of 1 is 1.0
